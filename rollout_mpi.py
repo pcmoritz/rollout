1eb72c1271a43556cea5a5d8642b8e138b1d0c73
Philipp Moritz <pcmoritz@gmail.com>
2018-04-29 19:33:59 -0700

    update

--- rollout_mpi.py
+++ rollout_mpi.py
@@ -5,8 +5,10 @@ import numpy as np
 
 from mpi4py import MPI
 
+NUM_ROLLOUTS_PER_WORKER = 100000
+
 def do_random_rollouts(env):
-    num_steps = random.randint(0, 100000)
+    num_steps = random.randint(0, NUM_ROLLOUTS_PER_WORKER)
     result = np.zeros((num_steps, 3))
     for i in range(num_steps):
         obs, rew, done, info = env.step([0])
@@ -49,4 +51,4 @@ rootdata = comm.gather(rollouts)
 # comm.Barrier()
 
 if rank == 0:
-    print("time was", time.time() - t1)
+    print("time was", size * NUM_ROLLOUTS_PER_WORKER / (time.time() - t1))
--- rollout_ray.py
+++ rollout_ray.py
@@ -4,6 +4,8 @@ import ray
 import random
 import time
 
+NUM_ROLLOUTS_PER_WORKER = 100000
+
 import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("num_workers", help="Number of workers to use",
@@ -16,7 +18,7 @@ print("args", args)
 def execute():
     env = gym.make("Pendulum-v0")
     env.reset()
-    num_steps = random.randint(0, 100000)
+    num_steps = random.randint(0, NUM_ROLLOUTS_PER_WORKER)
     result = np.zeros((num_steps, 3))
     for i in range(num_steps):
         obs, rew, done, info = env.step([0])
@@ -33,4 +35,4 @@ time.sleep(1.0)
 
 t1 = time.time()
 ray.get([execute.remote() for i in range(3*args.num_workers)])
-print("took", time.time() - t1)
+print("took", args.num_workers * NUM_ROLLOUTS_PER_WORKER / (time.time() - t1))
