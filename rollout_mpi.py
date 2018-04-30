bb18f1bd5470cee08f3fbc09fae2bfda95b285e4
Philipp Moritz <pcmoritz@gmail.com>
2018-04-29 19:08:20 -0700

    experiment version

--- rollout_mpi.py
+++ rollout_mpi.py
@@ -1,7 +1,19 @@
+import gym
 import time
 import random
+import numpy as np
 
 from mpi4py import MPI
+
+def do_random_rollouts(env):
+    num_steps = random.randint(0, 100000)
+    result = np.zeros((num_steps, 3))
+    for i in range(num_steps):
+        obs, rew, done, info = env.step([0])
+        if done:
+            obs = env.reset()
+        result[i, :] = obs
+    return result
 
 comm = MPI.COMM_WORLD
 
@@ -10,15 +22,31 @@ rank = comm.Get_rank()
 
 print(size, rank)
 
+env = gym.make("Pendulum-v0")
+env.reset()
+
 t1 = time.time()
 
-time.sleep(random.random())
+rollouts = do_random_rollouts(env)
 
-comm.Barrier()
+# time.sleep(random.random())
 
-time.sleep(random.random())
+rootdata = comm.gather(rollouts)
+# comm.Barrier()
 
-comm.Barrier()
+rollouts = do_random_rollouts(env)
+
+# time.sleep(random.random())
+
+rootdata = comm.gather(rollouts)
+# comm.Barrier()
+
+rollouts = do_random_rollouts(env)
+
+# time.sleep(random.random())
+
+rootdata = comm.gather(rollouts)
+# comm.Barrier()
 
 if rank == 0:
     print("time was", time.time() - t1)
--- rollout_ray.py
+++ rollout_ray.py
@@ -1,10 +1,21 @@
+import gym
+import numpy as np
 import ray
 import random
 import time
 
 @ray.remote
 def execute():
-    time.sleep(random.random())
+    env = gym.make("Pendulum-v0")
+    env.reset()
+    num_steps = random.randint(0, 100000)
+    result = np.zeros((num_steps, 3))
+    for i in range(num_steps):
+        obs, rew, done, info = env.step([0])
+        if done:
+            obs = env.reset()
+        result[i, :] = obs
+    return result
 
 ray.init(num_cpus=2)
 
@@ -13,5 +24,5 @@ ray.get([execute.remote()])
 time.sleep(1.0)
 
 t1 = time.time()
-ray.get([execute.remote() for i in range(4)])
+ray.get([execute.remote() for i in range(6)])
 print("took", time.time() - t1)
